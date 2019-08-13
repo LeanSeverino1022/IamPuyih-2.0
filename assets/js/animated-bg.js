4ee812ee8491b5fdb38b11d6d9322c138115d5b6
Puyih Severino <yihseverino@gmail.com>
2019-08-13 15:07:51 +0800

    update animation

--- assets/js/animated-bg.js
+++ assets/js/animated-bg.js
@@ -117,17 +117,20 @@ function runLogoAnimation(){
   // ANIMATED LOGO
   const svgPath = document.querySelectorAll('path');
 
-  const svgText = anime({
+  anime({
     targets: ['rect.logo-border'],
 
-    easing: 'easeInOutSine',
+    easing: 'easeInOutQuad',
     delay: 3000,
     duration: 1500,
     // direction: 'alternate',
     // loop: true, 
     opacity: 1,
     complete: function(){
-      $('body').removeClass('is-preload');
+           
+      setTimeout(() => {
+        $('body').removeClass('is-preload');
+      }, 500);     
     }
   });
 
@@ -146,23 +149,22 @@ function runLogoAnimation(){
 
     easing: 'easeInOutSine',
     duration: 700,
-    delay: 1500,
+    delay: 1000,
     strokeDashoffset: [anime.setDashoffset, 0],
   })
 
   const container = anime({
     targets: ['svg#my_logo'],
-
-    easing: 'easeInOutSine',
     rotate: [
       { value: -45, duration: 0, delay: 1100 },
       { value: 45, duration: 700 },
       { value: 0, duration: 700, delay: 1000 },
+      {value: '1turn', duration: 1000, delay: 1200}
     ],
     // scaling start with large font and scale to 1 when done
     scale: [
       { value: 2, delay: 0, duration : 1 },
-      { value: 1, delay: 4500, duration: 1000 },       
+      { value: 1, delay: 4500, duration: 1000},       
     ],
 
     opacity: 1
